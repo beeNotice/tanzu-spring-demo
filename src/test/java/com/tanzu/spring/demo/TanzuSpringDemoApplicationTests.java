000e7a9fc64c6699f5243d984561496421e51c3e
Fabien MARTIN <fmartin>
2022-10-27 15:19:15 +0200

    first commit

---
+++ src/test/java/com/tanzu/spring/demo/TanzuSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanzu.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanzuSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
